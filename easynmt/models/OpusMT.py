a505467634182c534d4bd4b08e9e120cf83257dc
Hugo Abonizio <hugo_abonizio@hotmail.com>
2021-01-29 12:32:07 +0000

    Add half precision inference

--- easynmt/models/OpusMT.py
+++ easynmt/models/OpusMT.py
@@ -10,16 +10,17 @@ logger = logging.getLogger(__name__)
 
 
 class OpusMT:
-    def __init__(self, model_path: str, max_loaded_models: int = 10):
+    def __init__(self, model_path: str, max_loaded_models: int = 10, fp16=False):
         self.model_path = model_path
         self.models = {}
+        self.fp16 = fp16
 
         with open(os.path.join(model_path, 'easynmt.json'), encoding='utf8') as fIn:
             self.config = json.load(fIn)
 
         self.max_loaded_models = max_loaded_models
 
-    def load_model(self, model_name):
+    def load_model(self, model_name, device):
         if model_name in self.models:
             self.models[model_name]['last_loaded'] = time.time()
             return self.models[model_name]['tokenizer'], self.models[model_name]['model']
@@ -28,6 +29,10 @@ class OpusMT:
             tokenizer = MarianTokenizer.from_pretrained(model_name)
             model = MarianMTModel.from_pretrained(model_name)
             model.eval()
+            model.to(device)
+
+            if self.fp16 and next(model.parameters()).is_cuda:
+                model.half()
 
             if len(self.models) >= self.max_loaded_models:
                 oldest_time = time.time()
@@ -43,8 +48,7 @@ class OpusMT:
 
     def translate_sentences(self, sentences: List[str], source_lang: str, target_lang: str, device: str, beam_size: int = 5, max_length: int = None, do_sample=False, top_k=50, top_p=1.0):
         model_name = 'Helsinki-NLP/opus-mt-{}-{}'.format(source_lang, target_lang)
-        tokenizer, model = self.load_model(model_name)
-        model.to(device)
+        tokenizer, model = self.load_model(model_name, device)
 
         inputs = tokenizer.prepare_seq2seq_batch(sentences, max_length=max_length, return_tensors="pt")
 
